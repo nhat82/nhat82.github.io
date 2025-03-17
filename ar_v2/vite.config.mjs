dc6a34edb03419b5eb823e007ca53bf7e7b99fd3
nhat82 <doristrungvuong@gmail.com>
2025-03-17 18:03:50 -0400

    changed path vite deployment

--- ar_v2/vite.config.mjs
+++ ar_v2/vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-    base: '/nhat82.github.io/ar_v2/', // Ensure this matches your GitHub Pages URL
+    base: 'ar_v2/', // Ensure this matches your GitHub Pages URL
     build: {
         outDir: '../docs/ar_v2',
         rollupOptions: {

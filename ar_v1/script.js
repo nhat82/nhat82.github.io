152a2836621efa5d1f284e4c77954db7f210ead7
nhat82 <doristrungvuong@gmail.com>
2025-03-25 17:34:12 -0400

    2

--- ar_v1/script.js
+++ ar_v1/script.js
@@ -25,7 +25,7 @@ window.onload = () => {
                             ...place,
                             distance: getDistance(userLat, userLon, place.lat, place.lon)
                         }))
-                        .filter(place => place.distance <= 1000) // Only within 5 meters
+                        .filter(place => place.distance <= 100) // Only within 5 meters
                         .sort((a, b) => a.distance - b.distance) // Sort nearest first
                         .slice(0, 10); // Pick the closest 10
 
@@ -38,9 +38,9 @@ window.onload = () => {
                         placeMarker.setAttribute('material', 'color: blue');
                         placeMarker.setAttribute('gps-entity-place', `latitude: ${place.lat}; longitude: ${place.lon};`);
                         placeMarker.setAttribute('text', `value: ${place.name}; align: center; color: white;`);
-                        placeMarker.addEventListener('loaded', () => {
-                            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-                        });
+                        // placeMarker.addEventListener('loaded', () => {
+                        //     window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                        // });
 
                         scene.appendChild(placeMarker);
                         // scene.appendChild(placeLabel);

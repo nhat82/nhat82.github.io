9e130289bdc899149802c30c11ddb1d1ab1b3c7b
nhat82 <doristrungvuong@gmail.com>
2025-03-26 15:38:14 -0400

    vf

--- ar_v1/draft.js
+++ ar_v1/draft.js
@@ -1,9 +1,10 @@
 window.onload = () => {
+    let testEntityAdded = false;
 
     const el = document.querySelector("[gps-new-camera]");
 
     el.addEventListener("gps-camera-update-position", e => {
-        
+        if(!testEntityAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
             // Add a box to the north of the initial GPS position
             const entity = document.createElement("a-box");
@@ -14,9 +15,11 @@ window.onload = () => {
             });
             entity.setAttribute('material', { color: 'red' } );
             entity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude,
+                latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude
             });
             document.querySelector("a-scene").appendChild(entity);
+        }
+        testEntityAdded = true;
     });
 };

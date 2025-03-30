52e7f33057739c7bfe1aed19ba02e3136f83576d
nhat82 <doristrungvuong@gmail.com>
2025-03-30 17:11:56 -0400

    vnd

--- hit-test/script.js
+++ hit-test/script.js
@@ -13,29 +13,29 @@ window.onload = () => {
 
             const userDot = document.getElementById('user-dot');
             userDot.setAttribute('gps-entity-place', `latitude: ${userLat}; longitude: ${userLon};`);
-            userDot.addEventListener('click', (ev) => {
-                ev.stopPropagation();
-                ev.preventDefault();
+            // userDot.addEventListener('click', (ev) => {
+            //     ev.stopPropagation();
+            //     ev.preventDefault();
         
-                const el = ev.detail?.intersection?.object?.el;
-                if (el && el === ev.target) {
-                    // Create and display a label with plant details
-                    const container = document.createElement('div');
-                    container.setAttribute('id', 'plant-label');
-                    container.style.position = 'absolute';
-                    container.style.top = '50%';
-                    container.style.left = '50%';
-                    container.style.transform = 'translate(-50%, -50%)';
-                    container.style.background = 'rgba(0, 0, 0, 0.7)';
-                    container.style.color = 'white';
-                    container.style.padding = '10px';
-                    container.style.borderRadius = '5px';
-                    container.style.fontSize = '14px';
-                    container.style.textAlign = 'center';
-                    container.style.zIndex = '1000';
+            //     const el = ev.detail?.intersection?.object?.el;
+            //     if (el && el === ev.target) {
+            //         // Create and display a label with plant details
+            //         const container = document.createElement('div');
+            //         container.setAttribute('id', 'plant-label');
+            //         container.style.position = 'absolute';
+            //         container.style.top = '50%';
+            //         container.style.left = '50%';
+            //         container.style.transform = 'translate(-50%, -50%)';
+            //         container.style.background = 'rgba(0, 0, 0, 0.7)';
+            //         container.style.color = 'white';
+            //         container.style.padding = '10px';
+            //         container.style.borderRadius = '5px';
+            //         container.style.fontSize = '14px';
+            //         container.style.textAlign = 'center';
+            //         container.style.zIndex = '1000';
         
-                    container.innerText = `User Clicked`;
-                }});
+            //         container.innerText = `User Clicked`;
+            //     }});
         },
         (error) => {
             console.error("Geolocation error:", error.message);

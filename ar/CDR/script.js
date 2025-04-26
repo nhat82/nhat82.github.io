35c789a4d13d21ea879f3b83e0191dffdfdc55d2
nhat82 <doristrungvuong@gmail.com>
2025-04-26 12:04:51 -0400

    8m

--- ar/CDR/script.js
+++ ar/CDR/script.js
@@ -90,7 +90,7 @@ window.addEventListener("load", () => {
             ...p,
             distance: getDistance(userLat, userLon, p.lat, p.lon),
           }))
-          .filter((p) => p.distance <= 10)
+          .filter((p) => p.distance <= 8)
           .sort((a, b) => a.distance - b.distance);
           // .slice(0, 10);
 
